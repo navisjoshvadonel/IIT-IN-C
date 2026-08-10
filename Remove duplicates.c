e967b455c23a28eef4afc923841159b2fe87dc1b
navis joshva donel J <navis.donel_bai28@mepcoeng.ac.in>
2026-08-10 15:39:09 +0530

    Add Remove duplicates.c solution

---
+++ Remove duplicates.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int removeDuplicates(int* nums, int numsSize) {
+    if (numsSize <= 2) {
+        return numsSize;
+    }
+    
+    int write_index = 2;
+    
+    for (int read_index = 2; read_index < numsSize; read_index++) {
+        if (nums[read_index] != nums[write_index - 2]) {
+            nums[write_index] = nums[read_index];
+            write_index++;
+        }
+    }
+    
+    return write_index;
+}
