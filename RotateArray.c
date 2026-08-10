5c08302efc4a2dc778f4162d3864693b7a9b0d5d
navis joshva donel J <navis.donel_bai28@mepcoeng.ac.in>
2026-08-10 14:46:39 +0530

    Add RotateArray.c solution

---
+++ RotateArray.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void reverse(int* nums, int s, int e) {
+    while (s < e) {
+        int temp = nums[s];
+        nums[s] = nums[e];
+        nums[e] = temp;
+        s++;
+        e--;
+    }
+}
+
+void rotate(int* nums, int numsSize, int k) {
+    k = k % numsSize; 
+    reverse(nums, 0, numsSize - 1);
+    reverse(nums, 0, k - 1);
+    reverse(nums, k, numsSize - 1);
+}
